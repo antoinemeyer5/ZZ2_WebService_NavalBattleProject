e0faa573b0af5ef7310a30914598d2dcdf2cf5bc
dummer25 <dummer.025@gmail.com>
2023-01-20 13:28:52 +0100

    Add business layer

--- Bataille Navale/Program.cs
+++ Bataille Navale/Program.cs
@@ -1,3 +1,5 @@
+using NavalWar.Business;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//add Your dependencies
+builder.Services.AddScoped<IGame, Game>();
 
 var app = builder.Build();
 
